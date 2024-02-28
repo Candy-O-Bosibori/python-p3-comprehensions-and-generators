abdbe62a7e3fdec2d1ca9ad55d49fc09d02bc73a
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-28 13:33:32 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers= [i for i in num_list if i%2 == 0]
+    return even_numbers
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 
 def make_exclamation(sentence_list):
-    pass
+    my_strings=[f"{i}!" for i in sentence_list ]
+    return my_strings
+    
+
+print (make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
